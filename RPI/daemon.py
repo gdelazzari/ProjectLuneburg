98565a2141283efce2428b03652d20993a2d3e2b
gdelazzari <giacky98.mail@gmail.com>
2017-06-15 13:54:32 +0200

    Comments on the main python program

--- RPI/daemon.py
+++ RPI/daemon.py
@@ -7,24 +7,17 @@ import serial
 import sunfish
 import sunfish_glue
 
+# Initialize the serial port
 ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.25)  # open serial port
 
+# Load/initialize the voice replacements database, which is used to
+# fix some misunderstandings of Google Voice
 replacements = db.loadDB('replacements')
 if replacements is None:
     replacements = []
     db.saveDB('replacements', replacements)
 
-'''
-replacements = [
-    ("invita", "in B3"),
-    ("Ingianni ", "in G"),
-    ("gianni ", "G"),
-    ("Gianni ", "G"),
-    ("bidone", "pedone"),
-    ("cinque", "5")
-]
-'''
-
+# This method allows to wait for the Arduino firmware to report the ready state
 def waitReady(timeout=15):
     print("[DEBUG] waiting for chessboard to be ready")
     start = time.time()
@@ -44,6 +37,12 @@ def waitReady(timeout=15):
         if tokens[1] == 'READY':
             return True
 
+# This method sends a move to the Arduino, in the form
+#  <piece>, <dx>, <dy>
+# and returns the real *raw* move which has been made on the board, which will
+# be in the form
+#  <sx>, <sy>, <dx>, <dy>
+# Both for the input and the returned move, a list/tuple is used
 def sendMove(move, timeout=3):
     string = "M" + str(move[0]) + str(move[1]) + str(move[2]) + '0'
     print("[SERIAL] send: \"" + string + "\"")
@@ -71,6 +70,11 @@ def sendMove(move, timeout=3):
             boardMove.append(int(tokens[3]))
             boardMove.append(int(tokens[4]))
             boardMove.append(int(tokens[5]))
+            # Check if the move is not valid
+            if any(n < 0 for n in boardMove):
+                # If it is not, the move probably is an elimination
+                # move, so not taken into consideration
+                continue
             return boardMove
         elif tokens[1] == 'ERR_WRONG_MOVE':
             return None
@@ -80,12 +84,20 @@ def sendMove(move, timeout=3):
             return None
     return boardMove
 
+# This method is similar to the one above, but instead sends a
+# raw move in the form:
+#  <sx>, <sy>, <dx>, <dy>
+# TODO: catch and return errors
 def sendRawMove(move):
     string = "R" + str(move[0]) + str(move[1]) + str(move[2]) + str(move[3])
     print("[SERIAL] send: \"" + string + "\"")
     ser.flushInput() # Clear the input buffer
     ser.write(string + '\n')
 
+# This method waits for the Arduino to report its move queue empty, meaning
+# that all the requested moves have been carried out
+# Returns False if a timeout occurred, otherwise returns True if the Arduino
+# completed the move within the specified time
 def waitMoveEnd(timeout=30):
     start = time.time()
     while True:
@@ -104,6 +116,8 @@ def waitMoveEnd(timeout=30):
         if tokens[1] == 'QE':
             return True
 
+# This method reads the board pieces matrix from the Arduino and returns it,
+# already converted/parsed/etc...
 def getMatrix():
     ser.flushInput()
     ser.write('X\n')
@@ -134,12 +148,16 @@ def getMatrix():
             printMatrix(matrix)
             return matrix
 
+# Quick method to print on screen the matrix read with the method above
 def printMatrix(m):
     for y in range(8):
         for x in range(8):
             print(m[x][y]),
         print("")
 
+# This method uses the voice replacements database (loaded at the beginning)
+# to fix a string caught by Google Voice Recognition (through the custom voice.py
+# library)
 def fixMove(moveStr):
     result = moveStr.lower()
     for r in replacements:
@@ -148,6 +166,11 @@ def fixMove(moveStr):
             result = result.replace(r[0], r[1])
     return result
 
+# This method parses a string move caught by the voice library (and eventually
+# fixed) into a move tuple in the form
+#  <piece>, <dx>, <dy>
+# The input string must be something like "pedone in B4". Syntax is really strict,
+# no flexibility is given.
 def parseMove(moveStr, player):
     tokens = moveStr.lower().split(" ")
     if len(tokens) != 3:
@@ -187,6 +210,8 @@ def parseMove(moveStr, player):
 
     return (piece, x, y)
 
+# This method asks the user on the console for a new replacement to add into
+# the database
 def acquireReplacement():
     print("[DEBUG] acquiring new replacement")
     print("[DEBUG] leave empty to ignore")
@@ -196,6 +221,9 @@ def acquireReplacement():
         replacements.append((tokens[0], tokens[1]))
         db.saveDB('replacements', replacements)
 
+# This method vocally asks the specified player (black/white) for its move,
+# and then returns the parsed move already in the form
+#  <piece>, <dx>, <dy>
 def askMove(player):
     talkStr = "Turno del giocatore "
     if player == "black":
@@ -221,6 +249,25 @@ def askMove(player):
             print("[MOVES] parsed move:", move)
             return move
 
+# This method is called every time the main loop needs to know what the move
+# of one player will be. This method wraps up both the vocal part and the Sunfish
+# autoplay algorithm, meaning that depending on <player_type> ('p' or 'c') this
+# function will either call the method above to ask the move vocally, or query
+# the algorithm for a computer generated move. This translates to a layer of
+# abstraction for the main loop that makes it possible not worry about whether a
+# player is a human or a computer.
+# <player> is either 'black' or 'white'
+# <player_type> is either 'p' (human) or 'c' (computer)
+# <pos> is a sunfish.Position object representing the game state
+# <searcher> is a sunfish.Searcher object associated with <pos>
+# Returns:
+#  <move>, <pos>
+# where <move> is the move either in the form
+#  <piece>, <dx>, <dy>
+# OR
+#  <sx>, <sy>, <dx>, <dy>
+# based on the player_type
+# while the returned <pos> is the updated position after the move
 def getMove(player, player_type, pos=None, searcher=None):
     if player_type == 'p':
         return askMove(player), pos
@@ -245,6 +292,10 @@ def getMove(player, player_type, pos=None, searcher=None):
     else:
         return None, pos
 
+# This method is used simply to reduce the code in the main loop
+# It simply gets the move for the specified player, sends it to the Arduino
+# and eventually updates the sunfish.Position <pos> with the Arduino parsed move.
+# All of this is done while checking for errors.
 def doPlayer(player, player_type, pos=None, searcher=None):
     print("[doplayer] pos=" + str(pos) + " searcher=" + str(searcher))
     while True:
@@ -265,6 +316,7 @@ def doPlayer(player, player_type, pos=None, searcher=None):
         if pos is not None: sunfish.print_pos(pos)
         return pos
 
+# Main game method, containing the game loop
 def game(mode):
     white_type = 'p'
     black_type = 'p'
@@ -286,15 +338,13 @@ def game(mode):
         pos = doPlayer('white', white_type, pos=pos, searcher=searcher)
         pos = doPlayer('black', black_type, pos=pos, searcher=searcher)
 
+# Main program
 if __name__ == "__main__":
-    voice.init()
+    voice.init() # Init voice engine (speech recognition and TTS)
 
-    waitReady()
+    waitReady() # Wait for the firmware to report ready state
 
-    time.sleep(1)
-
-    getMatrix()
-
+    # Ask for the playing mode
     voice.talk("Benvenuto, quale modalità vuoi attivare?")
     voice.play("sounds/ready.mp3")
 
@@ -314,6 +364,8 @@ if __name__ == "__main__":
             voice.talk("Non è una modalità valida, riprova")
             voice.play("sounds/ready.mp3")
 
+    # Enter the game in the specified move
     game(mode)
 
+    # Close the serial at the end
     ser.close()
